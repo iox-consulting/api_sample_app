7d071e63121502b70968a4530559f25e281ed99a
Izak Blom <izaklefleur@gmail.com>
2021-04-22 17:16:14 +0200

    Create quote request sample

---
+++ iox_sample_app/iox_sample_app/Controllers/InstructionsController.cs
@@ -0,0 +1,63 @@
+﻿using iox_sample_app.Requests;
+using iox_sample_app.Requests.Enums;
+using iox_sample_app.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace iox_sample_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructionsController : ControllerBase
+    {
+        private readonly IAPIService _apiService;
+        public InstructionsController(IAPIService apiService)
+        {
+            _apiService = apiService ?? throw new ArgumentNullException(nameof(apiService));
+        }
+
+        //This request creates a new quote
+        [HttpGet("CreateQuote")]
+        public async Task<IActionResult> CreateQuote()
+        {
+            try
+            {
+                //PLEASE NOTE THE REQUEST'S DATA IS TEST DATA AND SHOULD NOT BE POSTED TO THE API
+                var request = new CreateQuoteRequest()
+                {
+                    referenceId = "text1",
+                    accountReference = "sampleaccountref",
+                    Address = new CreateQuoteRequestAddress
+                    {
+                        City = "Pretoria",
+                        ContactEmail = "text@test.com",
+                        ContactNumber = "0224448788",
+                        ContactPerson = "Someone Interseting",
+                        District = "Gauteng",
+                        PostalCode = "3333",
+                        SecondaryContactNumber = "0228388383",
+                        StreetName = "Wisteria Avenue",
+                        StreetNumber = "33",
+                        Suburb = "Hennopsspark"
+                    },
+                    vehicleRegisterNumbers = new System.Collections.Generic.List<string>
+                    {
+                        "BDW701X",
+                        "RRW136W"
+                    }
+                };
+                var response = await _apiService.Post(request, RequestTypes.CreateQuote);
+
+                if (response.status == "Success")
+                    return Ok(response);
+                else
+                    return BadRequest(response.errors);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ iox_sample_app/iox_sample_app/Requests/CreateQuoteRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iox_sample_app.Requests
+{
+    public class CreateQuoteRequest
+    {
+        public string referenceId { get; set; }
+        public string accountReference { get; set; }
+        public List<string> vehicleRegisterNumbers { get; set; }
+        public CreateQuoteRequestAddress Address { get; set; }
+    }
+
+    public class CreateQuoteRequestAddress
+    {
+        public string StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string Suburb { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactNumber { get; set; }
+        public string SecondaryContactNumber { get; set; }
+    }
+}
